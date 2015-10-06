1e8195cd88d79076d3fc371b67bbc43b72a2f553
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-10-06 11:33:05 -0600

    able to show kitten bio when clicking kitten name

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var app = angular.module('Kittens', []);
+var app = angular.module('Kittens', ['ui.router']);
+
+  app.config(function ($stateProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'index'
+    })
+    // .state('show', {
+    //   url:'/kittens/:id',
+    //   templateUrl: '',
+    // })
+  })
