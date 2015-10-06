7ac18cc0e9858de24fbfe89a5987f3b8e2c6eb03
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-10-06 10:20:11 -0600

    able to display results

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-var app = angular.module('myTracker', []);
-app.controlller('MainController', function ($scope) {
-  $scope.showForm = false;
-  $scope.toggleForm = function () {
-    $scope.showForm = !$scope.showForm;
-  }
-  // $scope.submitPost = function () {
-  //   $scope.posts.push($scope.post);
-  //   $scope.post = {};
-  // }
-})
+var app = angular.module('Kittens', []);
